525cd67a2e01b625422d110dc9b4d46ebc105e4e
S Anil Kumar <somallaanil343@gmail.com>
2026-07-04 12:56:14 +0530

    leet code soultion

--- Functions/Le1108.py
+++ Functions/Le1108.py
@@ -1,10 +1,9 @@
 def anil(s):
     ans = ""
     for i in s:
-        ch = i
-        if ch == ".":
+        if i == ".":
             ans = ans + "[.]"
         else:
-            ans = ans + ch
+            ans = ans + i
     return ans
 print(anil("1.1.1.1"))
