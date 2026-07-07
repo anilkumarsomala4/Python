31b2db0d433354b031e1ffb66fecc31ea708f7ff
S Anil Kumar <somallaanil343@gmail.com>
2026-07-08 00:37:33 +0530

    finding maximum number in list

---
+++ Arrays/maxnumber.py
@@ -0,0 +1,6 @@
+nums = [12, 45, 7, 89, 23]
+max = 0
+for i in nums:
+    if i > max:
+        max = i
+print(max)
---
+++ Arrays/sumoflist.py
@@ -0,0 +1,5 @@
+nums = [5, 8, 2, 10]
+sum = 0
+for i in nums:
+    sum = sum + i
+print(sum)
