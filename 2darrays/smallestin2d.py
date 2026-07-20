d77013280ef91a92822b9f4a41c9c9d8b8a5d9a9
S Anil Kumar <somallaanil343@gmail.com>
2026-07-20 23:38:53 +0530

    smallest element

---
+++ 2darrays/smallestin2d.py
@@ -0,0 +1,13 @@
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+r = len(matrix)
+c=len(matrix[0])
+smallest = 99
+for i in range(r):
+    for j in range(c):
+        if matrix[i][j] < smallest:
+            smallest = matrix[i][j]
+print(smallest)
