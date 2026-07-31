0e8de0d1014ca7d8e198eb0645a4bc82f607cfa8
S Anil Kumar <somallaanil343@gmail.com>
2026-07-31 20:52:59 +0530

    added file of pattern7

---
+++ Patterns/pattern7.py
@@ -0,0 +1,9 @@
+n=6
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(" ",end=" ")
+    for k in range(i,0,-1):
+        print(k,end=" ")
+    for l in range(2,i+1):
+        print(l,end=" ")
+    print()
