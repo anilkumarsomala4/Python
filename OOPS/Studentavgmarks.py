8f92c8a5b9da2d7855e419106841f0fca96300e8
S Anil Kumar <somallaanil343@gmail.com>
2026-08-06 18:00:58 +0530

    I completed my task

---
+++ OOPS/Studentavgmarks.py
@@ -0,0 +1,16 @@
+class Student:
+    def __init__(self,name,marks):
+        self.name = name
+        self.marks= marks
+    def avg(self):
+        sum = 0
+        for i in self.marks:
+            sum = sum + i
+        a = sum // 3
+        print(self.name,"Your Avg Score is ",a)
+s1=Student("Anil",[86,98,85])
+s2=Student("Dhana sri",[98,77,97])
+s1.avg()
+s2.avg()
+
+        
